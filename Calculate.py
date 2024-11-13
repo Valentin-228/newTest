ea0b7dc2676c8c3e49af728741e9dfdf8649dc80
orb49 <orb49@mail.ru>
2024-11-13 19:12:31 +0300

    Добавление функции

--- Calculate.py
+++ Calculate.py
@@ -1,25 +1,31 @@
-try:
+def main():
+ try:
     result = input("Введите выражение: ")
     parts = result.split()
     if len(parts) == 3:
-        num1 = float(parts[0])
+        num1 = int(parts[0])
         operator = parts[1]
-        num2 = float(parts[2])
+        num2 = int(parts[2])
 
 
     if (operator == "+" and (1 <= num1 <= 10) and (1 <= num2 <= 10)):
-        print(int(num1 + num2))
+        result = str(num1 + num2)
     elif (operator == "-" and (1 <= num1 <= 10) and (1 <= num2 <= 10)):
-        print(int(num1 - num2))
+        result = str(num1 - num2)
     elif (operator == "*" and (1 <= num1 <= 10) and (1 <= num2 <= 10)):
-        print(int(num1 * num2))
+        result = str(num1 * num2)
     elif (operator == "/" and (1 <= num1 <= 10) and (1 <= num2 <= 10)):
-        print(int(num1 / num2))
+        result = str(num1 / num2)
     else:
         print("Только числа от 0 до 10")
-except NameError:
+ except NameError:
     print("Строка не является математической операцией")
+ except ValueError:
+    print("Только целые числа")
+ print(result)
+ print(type(result))
 
+main()
 
 
 
